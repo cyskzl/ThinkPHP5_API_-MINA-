11b70ae13c592c3ef621ac7317ed1d2cb22da595
cyskzl <cyskzl@gmail.com>
2017-08-22 03:23:29 +0800

    '初始化项目'

---
+++ application/extra/wechat.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2017/8/19
+ * Time: 6:22
+ */
+
+return [
+    'app_id' => '你的小程序appid',
+    'app_secret' => '你的小程序密钥',
+    'login_url' => "https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code"
+
+];
